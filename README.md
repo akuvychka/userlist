d2f1a8736bf818bdb2b369c5fc5d1e45af6c3089
Artem Kuvychka <artem.kuvychka@onelogin.com>
2020-03-28 23:06:56 +0200

    readme and response fixeses

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # userlist
 test user list rails and react
+
+#server
+use following commands to start
+
+> bundle install
+
+> yarn install --check-files
+
+> rake db:create db:migrate db:seed
+
+> rails s
+
+#client
+use following commands to start
+
+> npm install
+
+> npm start
+
+#for authentication
+
+Add key to cookies:
+
+**key**: _user_
+
+**value**: _form 1 to 10_ 
--- server/app/controllers/api/users_controller.rb
+++ server/app/controllers/api/users_controller.rb
@@ -1,11 +1,11 @@
 module Api
   class UsersController < ApplicationController
     def index
-      render json: { users: User.get_list }, status: :ok
+      render json: { users: User.get_list }, status: :ok if @user.present?
     end
 
     def me
-      render json: { me:  @user, mappings: @user.subscriber_user_mappings.select('id, user_id') }, status: :ok
+      render json: { me:  @user, mappings: @user.subscriber_user_mappings.select('id, user_id') }, status: :ok if @user.present?
     end
   end
 end
